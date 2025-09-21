1ce642a7c68fa1189da796c9e50ea345f1c79a12
LetiSouza18 <leticiagrandprime@gmail.com>
2025-09-21 15:13:39 -0300

    pass: the first test

--- velha.cpp
+++ velha.cpp
@@ -15,6 +15,18 @@
 
 int VerificaVelha( int velha[3][3] )
 {
+	int soma = 0;
+
+	for (int i = 0; i < 3; i++) {
+		if (velha[i][2] == 1) {
+			soma += 1;
+		}
+	}
+
+	if (soma == 3) {
+		return 1;
+	}
+
 	return 0; /*!< retorna zero para teste */ 
 }
 
