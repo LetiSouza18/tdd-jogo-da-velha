74c484b6b13574e26efb68c3a26ce6b7a3c59467
LetiSouza18 <leticiagrandprime@gmail.com>
2025-09-22 20:00:40 -0300

    pass: 'vitória x na segunda coluna da vertical' test

--- velha.cpp
+++ velha.cpp
@@ -39,6 +39,18 @@ int VerificaVelha( int velha[3][3] )
 		return 1;
 	}
 
+	soma = 0;
+
+	for (int i = 0; i < 3; i++) {
+		if (velha[i][1] == 1) {
+			soma += 1;
+		}
+	}
+
+	if (soma == 3) {
+		return 1;
+	}
+
 	return 0; /*!< retorna zero para teste */ 
 }
 
