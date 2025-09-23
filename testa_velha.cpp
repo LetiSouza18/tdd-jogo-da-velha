38d72d78d4d888aeba6792a5b7fcade1d9891092
LetiSouza18 <leticiagrandprime@gmail.com>
2025-09-23 08:45:41 -0300

    create: 'vitória x nas linhas horizontais' tests

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -30,3 +30,27 @@ TEST_CASE("Vitória x na segunda coluna da vertical", "[vitória][X]") {
                     };
   REQUIRE(VerificaVelha(teste1) == 1);
 }
+
+TEST_CASE("Vitória x na primeira linha da horizontal", "[vitória][X]") {
+  int teste1[3][3]= {   { 1, 1, 1 },
+                        { 0, 2, 0 },
+                        { 2, 0, 2 }
+                    };
+  REQUIRE(VerificaVelha(teste1) == 1);
+}
+
+TEST_CASE("Vitória x na segunda linha da horizontal", "[vitória][X]") {
+  int teste1[3][3]= {   { 2, 2, 0 },
+                        { 1, 1, 1 },
+                        { 0, 1, 2 }
+                    };
+  REQUIRE(VerificaVelha(teste1) == 1);
+}
+
+TEST_CASE("Vitória x na terceira linha da horizontal", "[vitória][X]") {
+  int teste1[3][3]= {   { 2, 0, 2 },
+                        { 0, 2, 0 },
+                        { 1, 1, 1 }
+                    };
+  REQUIRE(VerificaVelha(teste1) == 1);
+}
