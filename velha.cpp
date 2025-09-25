6a9e8b5dcf56e77a875b90ab1e6a934eac6af493
LetiSouza18 <leticiagrandprime@gmail.com>
2025-09-24 22:20:51 -0300

    fix: correct code using cpp lint

--- velha.cpp
+++ velha.cpp
@@ -36,7 +36,7 @@ int VerificaVelha(int velha[3][3]) {
   int quantidadeO = 0;
 
   // o loop vai iterar por todos os elementos da matriz
-  // garante que o número de jogadas seja válido, de forma que é impossível um jogador ter duas ou mais jogadas a mais do que o outro
+  // garante que um jogador não tenha duas ou mais jogadas do que o outro
 
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
@@ -53,11 +53,10 @@ int VerificaVelha(int velha[3][3]) {
   if (abs(quantidadeX - quantidadeO) > 1) {
     return -2;
   }
-  
+
   // o loop vai acontecer três vezes para verificar todas as linhas e colunas
 
   for (int i = 0; i < 3; i++) {
-
     // verifica se os três dígitos na linha i são iguais
 
     if (velha[i][0] == velha[i][1] && velha[i][1] == velha[i][2]) {
@@ -73,12 +72,14 @@ int VerificaVelha(int velha[3][3]) {
 
   // verifica se os três dígitos na primeira diagonal são iguais
 
-  int primeiraDiagonal = VerificaDiagonal(velha[0][0], velha[1][1], velha[2][2]);
+  int primeiraDiagonal = VerificaDiagonal(velha[0][0],
+                                          velha[1][1],
+                                          velha[2][2]);
 
   if (primeiraDiagonal) return primeiraDiagonal;
 
   // verifica se os três dígitos na segunda diagonal são iguais
-    
+
   int segundaDiagonal = VerificaDiagonal(velha[0][2], velha[1][1], velha[2][0]);
 
   if (segundaDiagonal) return segundaDiagonal;
